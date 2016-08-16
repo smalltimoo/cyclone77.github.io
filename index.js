8264150810eb186276bc044c2d48b375185c9b84
Cyclone77 <fillchar@live.com>
2016-08-16 11:25:31 +0800

    主页

---
+++ index.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    //滚动条
+    //$("body").jScrollPane({ showArrows: true });
+    //$(window).resize(function () {
+    //    $("body").jScrollPane({ showArrows: true });
+    //})
+    //锚点
+    $(".titleCon a").click(function () {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top - 50 + "px"
+        }, {
+            duration: 500,
+            easing: "swing"
+        });
+        return false;
+    });
+})
